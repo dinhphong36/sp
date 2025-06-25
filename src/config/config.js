0a7c6353537f69aa3548d597cca5cfd9cbf1f322
dinhphong36 <buidinhphong0306@gmail.com>
2025-06-26 02:39:31 +0700

    Cập nhật

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 async function connectDB() {
    try {
-        await mongoose.connect("mongodb+srv://buidinhphong:phong12345@cluster0.zpols9g.mongodb.net/mit_vietnam",{
+        await mongoose.connect(process.env.MONGODB_URI,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
